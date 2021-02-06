fa196ea980e62595692b319fd88c028484e72ed2
Juan Cedeño <juancc0315@gmail.com>
2021-02-06 14:51:03 +0800

    Dc page finished

--- src/pages/DcPage.tsx
+++ src/pages/DcPage.tsx
@@ -1,10 +1,21 @@
+import { useContext } from "react"
+import { DcList } from "../components/DcList"
+import { HeroeContext } from "../context/HeroeContext"
 
 
 export const DcPage = () => {
 
+     const {heroe} = useContext(HeroeContext)
+
+
+
      return (
           <div className = 'cont-heroe'>
-              
+              {
+                   heroe.map(items => (
+                        <DcList items = {items} key = {items._id}/>
+                   ))
+              }
           </div>
      )
 }
